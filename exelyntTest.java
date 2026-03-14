9d23716debc7d35efece56e49b947558ce3260eb
Vishal Pawar <mr.vishu4912@gmail.com>
2026-03-14 13:23:11 +0530

    Add files via upload

---
+++ exelyntTest.java
@@ -0,0 +1,23 @@
+package com.exelynt;
+
+public class exelyntTest {
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+
+			int num = i % 2;
+
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print(num + " ");
+				num = 1 - num;
+
+			}
+
+			System.out.println();
+		}
+	}
+
+}
